16e180769cd1b2b88d8f93edf5d31acd58bc93ac
TheSwagVader <egorzverev666@gmail.com>
2020-07-24 20:24:02 +0300

    add ram

--- GRE/gervi_exceptions.py
+++ GRE/gervi_exceptions.py
@@ -1,7 +1,16 @@
 from enum import Enum
-
-errorLevels = Enum(WARNING, ERROR, CRITICAL, FATAL)
+from abc import abstractmethod
+class ErrorLevels(Enum):
+    WARNING, ERROR, CRITICAL, FATAL = range(4)
 class GerviThrowable:
     @abstractmethod
     def throw(self):
         pass
+
+class GerviError(GerviThrowable):
+    def __init__(self, errorName, errorMsg, errorLevel):
+        self.__errorName = errorName
+        self.__errorMsg = errorMsg
+        self.__errorLevel = errorLevel
+    
+    #def throw(self):
