5a9d487cb84e9300dd93682cea9278d2ac5742ff
TheSwagVader <egorzverev666@gmail.com>
2020-08-01 17:19:08 +0300

    add gervi_vm.py - a Gervi VM lib

--- GRE/gervi_vm.py
+++ GRE/gervi_vm.py
@@ -1,21 +1,22 @@
-#from gervi_vmf import VirtualMachineFile as VMF
-#from gervi_workingmem import Work
+from gervi_vmf import VirtualMachineFile
+from gervi_workingmem import WorkingMemory
+from gervi_ram import RandomAccessMemory
 
 
-#class VirtualMachine:
-#    def __init__(self, file):
-#        vmf = VMF()
-#        vmf.load(file)
-#        self.properties = vmf.getProperties()
-#        self.workingMem = [
-#            [binmath.BinaryData(VMF.getint('MEMORY', 'SegmentSize')) for i in range(VMF.getint('WM', 'Width'))]
-#            for j in range(VMF.getint('WM', 'Heigth'))
-#            ]
-#        self.RAM = [
-#            [binmath.BinaryData(VMF.getint('MEMORY', 'SegmentSize')) for i in range(VMF.getint('RAM', 'Width'))]
-#            for j in range(VMF.getint('RAM', 'Heigth'))
-#            ]
-#        self.statusCode = 0
-#    
-#    def run(self):
-#        pass
+class VirtualMachine:
+    def __init__(self, file):
+        vmf = VirtualMachineFile()
+        vmf.load(file)
+        properties = vmf.getProperties()
+        self.__name = properties['name']
+        self.__author = properties['author']
+        self.__email = properties['email']
+        self.__version = properties['version']
+        self.__desc = properties['desc']
+        self.__segmentSize = properties['segsize']
+        self.__workingMemory = WorkingMemory(self.__segmentSize, properties['wmsizeh'], properties['wmsizev'])
+        self.__RAM = RandomAccessMemory(properties['ramsizeh'], properties['ramsizev'])
+        self.__statusCode = 0
+    
+    def run(self):
+        print('It works!')
